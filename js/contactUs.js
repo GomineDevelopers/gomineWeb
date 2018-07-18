994b37bcbe6bdd07d77a43c99bd9239bf38cd6c8
Arron <1274294199@qq.com>
2018-07-18 08:13:09 +0800

    update

--- js/contactUs.js
+++ js/contactUs.js
@@ -1,18 +1,12 @@
 // //创建和初始化地图函数：
-
-
-
 $(document).ready(function () {
   /*初始化*/
-  var map = new BMap.Map("container", {
-    minZoom: 20
-  }); // 创建地图实例
-
+  var map = new BMap.Map("container"); // 创建地图实例
   var point = new BMap.Point(116.499868, 39.921244); // 创建中心点坐标
   map.centerAndZoom(point, 20); // 初始化地图，设置中心点坐标和地图级别
   map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
-  var myGeo = new BMap.Geocoder(); // 创建地址解析器实例
-
+  map.setCurrentCity("北京");
+  var myGeo = new BMap.Geocoder(); // 创建地址解析器实例  
   /*添加控件*/
   map.addControl(new BMap.ScaleControl()); //比例尺
   // 地理位置信息切换
@@ -27,14 +21,15 @@ $(document).ready(function () {
     height: 80, // 信息窗口高度
     title: "所在区域", // 信息窗口标题
   };
-
+  
+  
   $('#bg-bj').click(function () {
-    map.setCenter(new BMap.Point(116.499868, 39.921244))
+    map.panTo(new BMap.Point(116.499868, 39.921244))
     var markerOne = new BMap.Marker(new BMap.Point(116.499868, 39.921244)); // 创建标注
-    var content = "地址：北京市 朝阳区 住邦2000商务楼 1号楼B702-703";
-    map.addOverlay(markerOne); // 将标注添加到地图中
-    var infoWindow = new BMap.InfoWindow(content, opts); // 创建信息窗口对象 
-    map.openInfoWindow(infoWindow, new BMap.Point(116.499868, 39.921244)); //开启信息窗口
+  var content = "地址：北京市 朝阳区 住邦2000商务楼 1号楼B702-703";
+  map.addOverlay(markerOne); // 将标注添加到地图中
+  var infoWindow = new BMap.InfoWindow(content, opts); // 创建信息窗口对象 
+  map.openInfoWindow(infoWindow, new BMap.Point(116.499868, 39.921244)); //开启信息窗口
     var flag = $(this);
     if (flag.hasClass('bg-bj-active')) {} else {
       flag.addClass('bg-bj-active');
@@ -43,11 +38,12 @@ $(document).ready(function () {
     }
   });
   $('#bg-jh').click(function () {
-    map.setCenter(new BMap.Point(119.671047, 29.137416))
+    // map.setCenter(new BMap.Point(119.671047, 29.137416))
+    map.panTo(new BMap.Point(119.671047, 29.137416));
     var markerTwo = new BMap.Marker(new BMap.Point(119.671047, 29.137416)); // 创建标注
-    var content = "金华市 婺城区 金华电子商务创 业园502-503";
+    var contentone = "金华市 婺城区 金华电子商务创 业园502-503";
     map.addOverlay(markerTwo); // 将标注添加到地图中
-    var infoWindow = new BMap.InfoWindow(content, opts); // 创建信息窗口对象 
+    var infoWindow = new BMap.InfoWindow(contentone, opts); // 创建信息窗口对象 
     map.openInfoWindow(infoWindow, new BMap.Point(119.671047, 29.137416)); //开启信息窗口
     var flag = $(this);
     if (flag.hasClass('bg-jh-active')) {} else {
@@ -58,7 +54,12 @@ $(document).ready(function () {
   });
 
   $('#bg-gy').click(function () {
-    map.setCenter(new BMap.Point(106.652481, 26.627495))
+    map.panTo(new BMap.Point(106.652481, 26.627495));
+    var markerThree = new BMap.Marker(new BMap.Point(106.652481, 26.627495)); // 创建标注
+    var contentTwo = "贵阳市 观山湖区 贵阳国家高新 技术产业开发区（毕节路）";
+    map.addOverlay(markerThree); // 将标注添加到地图中
+    var infoWindow = new BMap.InfoWindow(contentTwo, opts); // 创建信息窗口对象 
+    map.openInfoWindow(infoWindow, new BMap.Point(106.652481, 26.627495)); //开启信息窗口
     var flag = $(this);
     if (flag.hasClass('bg-gy-active')) {} else {
       flag.addClass('bg-gy-active');
